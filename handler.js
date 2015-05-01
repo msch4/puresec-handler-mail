6b171da549872719b91082fa8d00a754036fcf74
Martin <martin.scheidweiler@otto.de>
2015-05-01 20:51:33 +0200

    use smtp mailing service

--- handler.js
+++ handler.js
@@ -1,5 +1,41 @@
+var nodemailer = require('nodemailer');
+try {
+    var config = require('./mailconfig.json');
+    var recipients = require('./recipients.json');
+} catch (e) {
+    console.log("You must define both a mailconfig.json and a recipients.json for mailing information");
+    process.exit();
+}
+
 var handle = function () {
-  console.log("\n sending a mail that something evil happened");
+    var transporter = nodemailer.createTransport({
+        service: config.service,
+        auth: {
+            user: config.user,
+            pass: config.password
+        }
+    }),
+        addresses = recipients.recipients;
+
+    if (!!addresses) {
+        addresses.every(function (elem) {
+            console.log("\n Sending a mail to '%s'", elem);
+            transporter.sendMail({
+                from: 'puresecNotifications@puresec.com',
+                to: elem,
+                subject: 'CAUTION',
+                text: 'Something very evil happened!'
+            }, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Message sent: ' + info.response);
+                }
+            });
+        });
+    } else {
+        console.log("\n No recipients defined");
+    }
 };
 
 module.exports = {
